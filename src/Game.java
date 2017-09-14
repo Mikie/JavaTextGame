7b53357bcd12b9c6b18ac0a6ee555346603aa24a
Mikie <scebelmj@yahoo.com>
2017-09-13 20:02:45 -0400

    Added gui

---
+++ src/Game.java
@@ -0,0 +1,83 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+
+    Main main = new Main();
+
+
+    public static void main(String[] args) {
+
+
+        //System Variables
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        //game Variables
+        String[] enemies = {"Dave", "Richie", "Jordan", "Pixelator"};
+        int maxhealth = 85;
+        int enemyattackdamage = 35;
+
+        //player Variables
+        int health = 500;
+        int attackdamage = 70;
+        int healthpotionsnum = 5;
+        int potionhealamount = 40;
+        int potiondropchance = 60;
+        String[] teammates = {"Bob", "DJ", "Narwhal", "James", "Sean"};
+
+
+        boolean running = true;
+
+        System.out.println("Welcome to the game!" + "Would you like to play y/n");
+        String input = in.nextLine();
+        if (input.equals("n")) {
+            System.out.println("BYE");
+            return;
+        }
+
+        if (input.equals("y")) {
+            //Starts while loop
+            TEXTGAME:
+            while (running) {
+                System.out.println("----------------------------------------------------");
+                int enemyhealth = rand.nextInt();
+                String enemy = enemies[rand.nextInt(enemies.length)];
+                System.out.println("A while " + enemy + " Has showed up!");
+
+
+                while (maxhealth > 0) {
+                    System.out.println("Your HP: " + health);
+                    System.out.println("Enemy Max Health: " + maxhealth);
+                    System.out.println("What would you like to do?");
+                    System.out.println("1. Run away");
+                    System.out.println("2. Fight the enemy");
+                    System.out.println("3. Call backup");
+                    String inp = in.nextLine();
+                    if (inp.equals("1")) {
+                        System.out.println("You ran away like a pussy!");
+                        System.out.println("THE END");
+                        break;
+                    } else if (inp.equals("2")) {
+                        int damageGiven = rand.nextInt(attackdamage);
+                        int damageTaken = rand.nextInt(enemyattackdamage);
+                        maxhealth -= damageGiven;
+                        health -= damageTaken;
+                        System.out.println("You damaged " + enemy + " for " + damageGiven + " HP");
+                        System.out.println("However you took " + damageTaken + " HP");
+                        continue;
+                    } else if (inp.equals("3")) {
+                        String teammate = teammates[rand.nextInt(teammates.length)];
+                        System.out.println("You have called for back up and your teammate " + teammate);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+                    /* DO TO LIST:
+                    Add more to game
+                    make more stuff to do and make it more complex/fun.
+                     */
--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,75 @@
 
-import java.util.Random;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+
+    //TODO move stuff over here
+
+
+    Stage mainWindow;
+    Stage secondWindow;
+
+    Button confirmationButton;
+
+    Label inputLabel;
 
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        //System Variables
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
-        //game Variables
-        String[] enemies = {"Dave", "Richie", "Jordan", "Pixelator"};
-        int maxhealth = 85;
-        int enemyattackdamage = 35;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainWindow = primaryStage;
+        mainWindow.setTitle("TextGame");
 
-        //player Variables
-        int health = 500;
-        int attackdamage = 70;
-        int healthpotionsnum = 5;
-        int potionhealamount = 40;
-        int potiondropchance = 60;
-        String[] teammates = {"Bob", "DJ", "Narwhal", "James", "Sean"};
+        //Input and labels
+        TextField userInput = new TextField();
+        userInput.setMaxWidth(200);
+        inputLabel = new Label();
 
+        confirmationButton = new Button();
+        confirmationButton.setText("Confirm");
+        confirmationButton.setOnAction(this); // for the ActionEvent below
 
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        VBox vBox = new VBox(10, userInput, confirmationButton, hBox);
+        vBox.setAlignment(Pos.CENTER);
 
-        boolean running = true;
+        inputLabel.textProperty().bind(userInput.textProperty());
 
-        System.out.println("Welcome to the game!" + "Would you like to play y/n");
-        String input = in.nextLine();
-        if (input.equals("n")) {
-            System.out.println("BYE");
-            return;
-        }
+        Scene scene = new Scene(vBox, 300, 200);
+        mainWindow.setScene(scene);
+        mainWindow.show();
+    }
 
-        if (input.equals("y")) {
-            //Starts while loop
-            TEXTGAME:
-            while (running) {
-                System.out.println("----------------------------------------------------");
-                int enemyhealth = rand.nextInt();
-                String enemy = enemies[rand.nextInt(enemies.length)];
-                System.out.println("A while " + enemy + " Has showed up!");
+    @Override
+    public void handle(ActionEvent event) {
+        String userInput = inputLabel.getText();
+        if(event.getSource() == confirmationButton) {
+            //do your thing, fiddle about with it.
+            //i've made a simple second window to display your input :)
 
+            secondWindow = new Stage();
 
-                while(maxhealth > 0) {
-                    System.out.println("Your HP: " + health);
-                    System.out.println("Enemy Max Health: " + maxhealth);
-                    System.out.println("What would you like to do?");
-                    System.out.println("1. Run away");
-                    System.out.println("2. Fight the enemy");
-                    System.out.println("3. Call backup");
-                    String inp = in.nextLine();
-                    if (inp.equals("1")) {
-                        System.out.println("You ran away like a pussy!");
-                        System.out.println("THE END");
-                        break;
-                    }
-                    else if (inp.equals("2")){
-                        int damageGiven = rand.nextInt(attackdamage);
-                        int damageTaken = rand.nextInt(enemyattackdamage);
-                        maxhealth -= damageGiven;
-                        health -= damageTaken;
-                        System.out.println("You damaged " + enemy + " for " + damageGiven + " HP");
-                        System.out.println("However you took " + damageTaken + " HP");
-                        continue;
-                    }else if (inp.equals("3")){
-                        String teammate = teammates[rand.nextInt(teammates.length)];
-                        System.out.println("You have called for back up and your teammate " + teammate);
-                    }
-                }
-                break;
-            }
+            Label memes = new Label("Here was your input: " + userInput);
+            VBox box = new VBox(20, memes);
+            box.setAlignment(Pos.BASELINE_CENTER);
+
+            Scene scene = new Scene(box, 300, 20);
+            secondWindow.setScene(scene);
+            secondWindow.show();
         }
     }
 }
-                    /* DO TO LIST:
-                    Add more to game
-                    make more stuff to do and make it more complex/fun.
-                     */
