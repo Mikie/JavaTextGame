5d2a669f0fbe00e5ffab7becedd5352b09f8d2c8
Aerh <callum.aero@gmail.com>
2017-10-26 19:49:30 +0100

    Added myself and fixed formatting

--- src/Game.java
+++ src/Game.java
@@ -14,7 +14,7 @@ public class Game {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
         //game Variables
-        String[] enemies = {"Dave", "Richie", "Jordan", "Pixelator"};
+        String[] enemies = {"Dave", "Richie", "Jordan", "KnightDev", "Pixelator"};
         int maxhealth = 85;
         int enemyattackdamage = 35;
 
@@ -24,7 +24,7 @@ public class Game {
         int healthpotionsnum = 5;
         int potionhealamount = 40;
         int potiondropchance = 60;
-        String[] teammates = {"Bob", "DJ", "Narwhal", "James", "Sean"};
+        String[] teammates = {"Bob", "DJ", "Narwhal", "James", "Sean", "Aerh"};
 
 
         boolean running = true;
@@ -46,6 +46,7 @@ public class Game {
                 System.out.println("A while " + enemy + " Has showed up!");
 
 
+                label:
                 while (maxhealth > 0) {
                     System.out.println("Your HP: " + health);
                     System.out.println("Enemy Max Health: " + maxhealth);
@@ -54,21 +55,23 @@ public class Game {
                     System.out.println("2. Fight the enemy");
                     System.out.println("3. Call backup");
                     String inp = in.nextLine();
-                    if (inp.equals("1")) {
-                        System.out.println("You ran away like a pussy!");
-                        System.out.println("THE END");
-                        break;
-                    } else if (inp.equals("2")) {
-                        int damageGiven = rand.nextInt(attackdamage);
-                        int damageTaken = rand.nextInt(enemyattackdamage);
-                        maxhealth -= damageGiven;
-                        health -= damageTaken;
-                        System.out.println("You damaged " + enemy + " for " + damageGiven + " HP");
-                        System.out.println("However you took " + damageTaken + " HP");
-                        continue;
-                    } else if (inp.equals("3")) {
-                        String teammate = teammates[rand.nextInt(teammates.length)];
-                        System.out.println("You have called for back up and your teammate " + teammate);
+                    switch (inp) {
+                        case "1":
+                            System.out.println("You ran away like a pussy!");
+                            System.out.println("THE END");
+                            break label;
+                        case "2":
+                            int damageGiven = rand.nextInt(attackdamage);
+                            int damageTaken = rand.nextInt(enemyattackdamage);
+                            maxhealth -= damageGiven;
+                            health -= damageTaken;
+                            System.out.println("You damaged " + enemy + " for " + damageGiven + " HP");
+                            System.out.println("However you took " + damageTaken + " HP");
+                            break;
+                        case "3":
+                            String teammate = teammates[rand.nextInt(teammates.length)];
+                            System.out.println("You have called for back up and your teammate " + teammate);
+                            break;
                     }
                 }
                 break;
@@ -76,7 +79,6 @@ public class Game {
         }
     }
 }
-
                     /* DO TO LIST:
                     Add more to game
                     make more stuff to do and make it more complex/fun.
